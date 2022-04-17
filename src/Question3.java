f1aacb5ddb73015f91c811eacf8966e4651ee9ea
Utsavraut <raututsav35@gmail.com>
2022-04-17 21:01:20 +0545

    assignment

--- src/Question3.java
+++ src/Question3.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 //Write ternary operator for the question no. 1.
 public class Question3 {
     public static void main(String[] args) {
-        int age=20;
+        int age=21;
         String vote;
         vote =(age>18)? "Can vote " : "Cannot vote";
         System.out.println(vote);
