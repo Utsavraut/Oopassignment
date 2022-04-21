cd138dee3e67c6c9b0884ef63b4f0ba57877be8b
Utsavraut <raututsav35@gmail.com>
2022-04-21 10:36:09 +0545

    assignment

---
+++ src/Switch/Ques2.java
@@ -0,0 +1,27 @@
+package Switch;
+
+/*import java.util.Scanner;
+
+//JAVA Program to find the maximum between two numbers. Using the switch statement.
+public class Ques2 {
+    public static void main(String[] args) {
+
+        Scanner scanner=new Scanner(System.in);
+
+        System.out.println("Input Two Integer");
+         int a=scanner.nextInt();
+         int b=scanner.nextInt();
+        switch(a > b)
+        {
+            case 0:
+                System.out.println("maximum number " + b);
+                break;
+            case 1:
+                System.out.println("maximum number " + a);
+                break;
+            default:
+                System.out.println("Both are equal");
+        }
+
+    }
+}*/
--- src/ifelse/Ques12.java
+++ src/ifelse/Ques12.java
@@ -8,13 +8,20 @@ public class Ques12 {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter month number: ");
         int month = input.nextInt();
+        System.out.println("Enter year : ");
+        int year = input.nextInt();
         if (month==1||month==3||month==5||month==7||month==8||month==10||month==12) {
             System.out.println("No. of Days: 31");
         } else if (month==4||month==6||month==9||month==11) {
             System.out.println("No. of Days: 30");
         } else if (month==2) {
-            System.out.println("No. of Days: 28");
-        } else {
+            if ((year % 4000 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
+                System.out.println("No of days : 29");
+            else {
+                System.out.println("No of days :28");
+            }
+        }
+        else {
             System.out.println("Invalid month number!");
         }
     }
