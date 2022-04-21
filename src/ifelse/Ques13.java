9165e2bdba9d06486d389f6e18152c56ba5e628b
Utsavraut <raututsav35@gmail.com>
2022-04-21 10:47:13 +0545

    assignment

--- src/ifelse/Ques13.java
+++ src/ifelse/Ques13.java
@@ -1,4 +1,20 @@
 package ifelse;
+
+import java.util.Scanner;
+
 //Write a JAVA program to count total number of notes in given amount.
 public class Ques13 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter a value:");
+        int amount =scanner.nextInt();
+        int total;
+        if(amount%5==0){
+            total=amount/5;
+            System.out.println(total);
+        }else{
+            System.out.println("Invalid Figures");
+        }
+    }
+
 }
